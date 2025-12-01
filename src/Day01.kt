39a3b22ad70f85a249da9198802403045252a908
tre3p <whereareyo@yahoo.com>
2025-12-01 19:02:35 +0400

    day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,57 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    println(part1(readInput("day1")))
+    println(part2(readInput("day1")))
+}
+
+fun part1(input: List<String>): Int {
+    val acc = CircularInt()
+
+    return input.map { rotation ->
+        val rotationsCount = rotation.drop(1).toInt()
+
+        when (rotation[0]) {
+            'R' -> acc.apply(rotationsCount)
+            'L' -> acc.apply(-rotationsCount)
+            else -> error("Unexpected operation '${rotation[0]}'")
+        }
+    }.count { it == 0 }
+}
+
+fun part2(input: List<String>): Int {
+    val acc = CircularInt()
+
+    input.forEach { rotation ->
+        val rotationsCount = rotation.takeLastWhile { Character.isDigit(it) }.toInt()
+
+        when (rotation[0]) {
+            'R' -> acc.apply(rotationsCount)
+            'L' -> acc.apply(-rotationsCount)
+            else -> error("Unexpected operation '${rotation[0]}'")
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    return acc.wraps
+}
+
+class CircularInt(start: Int = 50, max: Int = 99) {
+    private val range = max.inc()
+
+    var value: Int = start
+        private set
+    var wraps: Int = 0
+        private set
+
+    fun apply(delta: Int): Int {
+        val before = value
+        val after = ((before + delta) % range + range) % range
+
+        wraps += if (delta > 0) {
+            (before + 1..before + delta).count { it % range == 0 }
+        } else if (delta < 0) {
+            (before + delta until before).count { (it % range + range) % range == 0 }
+        } else 0
+
+        value = after
+        return after
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
 }
