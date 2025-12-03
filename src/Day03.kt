6a0eb9f8d978fe0a41e2698a40ef6f26803dc475
tre3p <whereareyo@yahoo.com>
2025-12-03 19:33:43 +0400

    day3 part 2 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,5 +1,6 @@
 fun main() {
     println(part1(readInput("day3")))
+    println(part2(readInput("day3")))
 }
 
 private fun part1(input: List<String>): Int = input
@@ -7,9 +8,7 @@ private fun part1(input: List<String>): Int = input
         var first = 0
         var second = 0
 
-        bank.split("")
-            .filter { it.isNotEmpty() }
-            .map { it.toInt() }
+        bank.toIntSequence()
             .let { bankElements ->
                 bankElements.forEachIndexed { idx, bankElement ->
                     if (bankElement > first && idx != bankElements.lastIndex) {
@@ -23,3 +22,24 @@ private fun part1(input: List<String>): Int = input
 
         "$first$second".toInt()
     }
+
+private fun part2(input: List<String>) =
+    input.sumOf { bank ->
+        bank.toIntSequence()
+            .let { bankElements ->
+                var safeToDrop = bankElements.size - 12
+                val resultStack = ArrayDeque<Int>()
+
+                bankElements.forEach { el ->
+                    while (resultStack.isNotEmpty() && el > resultStack.last() && safeToDrop != 0) {
+                        resultStack.removeLast()
+                        safeToDrop--
+                    }
+                    resultStack.add(el)
+                }
+
+                resultStack.take(12).joinToString("").toLong()
+            }
+    }
+
+private fun String.toIntSequence() = this.split("").filter { it.isNotEmpty() }.map { it.toInt() }
